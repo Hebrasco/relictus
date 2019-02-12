6f92ce9499aa08ea1837571519b9746123988947
Daniel Bedrich <hebrasco@icloud.com>
2019-02-12 12:14:45 +0100

    Added baisc override functions
    Added basic override functions such as initGame(), initInout() etc., and onUpdate().

--- src/main/relictus/Main.java
+++ src/main/relictus/Main.java
@@ -1,6 +1,8 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 
+import java.util.Map;
+
 public class Main extends GameApplication {
 
     @Override
@@ -8,6 +10,31 @@ public class Main extends GameApplication {
         settings.setWidth(800);
         settings.setHeight(600);
         settings.setTitle("Relictus");
+    }
+
+    @Override
+    protected void initInput() {
+        super.initInput();
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        super.initGameVars(vars);
+    }
+
+    @Override
+    protected void initGame() {
+        super.initGame();
+    }
+
+    @Override
+    protected void initUI() {
+        super.initUI();
+    }
+
+    @Override
+    protected void onUpdate(double tpf) {
+        super.onUpdate(tpf);
     }
 
     public static void main(String[] args) {
