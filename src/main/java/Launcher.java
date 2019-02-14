5afcaaaf8615a6a96c3c6ad05eba490c8e23e18b
Markus Kremer <mkremer@seibert-media.net>
2019-02-14 12:21:13 +0100

    restructure in order to run launcher b4 game

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("assets/ui/launcher.fxml"));
+		Parent root = fxmlLoader.load();
+		Stage stage = new Stage();
+
+		stage.setTitle("RELICTUS Launcher");
+		stage.setScene(new Scene(root));
+		stage.setResizable(false);
+		stage.centerOnScreen();
+		stage.show();
+	}
+}
---
+++ src/main/java/Relictus.java
@@ -0,0 +1,40 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+import java.util.Map;
+
+public class Relictus extends GameApplication {
+
+	@Override
+	protected void initSettings(GameSettings settings) {
+		settings.setWidth(800);
+		settings.setHeight(600);
+		settings.setTitle("R E L I C T U S");
+		settings.setVersion("");
+	}
+
+	@Override
+	protected void initInput() {
+		super.initInput();
+	}
+
+	@Override
+	protected void initGameVars(Map<String, Object> vars) {
+		super.initGameVars(vars);
+	}
+
+	@Override
+	protected void initGame() {
+		super.initGame();
+	}
+
+	@Override
+	protected void initUI() {
+		super.initUI();
+	}
+
+	@Override
+	protected void onUpdate(double tpf) {
+		super.onUpdate(tpf);
+	}
+}
