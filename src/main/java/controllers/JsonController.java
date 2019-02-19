0e969a577d4f288ccf1cc11b69e5e9da5b3aa5dd
Daniel Bedrich <hebrasco@icloud.com>
2019-02-19 08:58:20 +0100

    Fixed jsonController singleton instantiation

--- src/main/java/controllers/JsonController.java
+++ src/main/java/controllers/JsonController.java
@@ -15,13 +15,16 @@ public class JsonController {
     public JSONObject json;
     private String textsJsonRelativePath = "/assets/json/texts.json";
     private URL textsJsonURL = getClass().getResource(textsJsonRelativePath);
-    private static JsonController jsonController = new JsonController( );
+    private static JsonController jsonController;
 
     private JsonController() {
 
     }
 
     public static JsonController getInstance( ) {
+        if (jsonController == null) {
+            jsonController = new JsonController( );
+        }
         return jsonController;
     }
 
