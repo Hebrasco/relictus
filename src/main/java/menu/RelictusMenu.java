bc9ca69a5dd4b9449d23b7888af59b84a290f4cc
Daniel Bedrich <hebrasco@icloud.com>
2019-02-20 18:16:31 +0100

    Added some TODOs

--- src/main/java/menu/RelictusMenu.java
+++ src/main/java/menu/RelictusMenu.java
@@ -300,6 +300,7 @@ public class RelictusMenu extends FXGLMenu {
         return controlsMenuButton;
     }
 
+    // TODO: Sprache dropdown Menü aus video Einstellungen entfernen (eigene implementation von "createContentVideo()" erstellen)
     private MenuButton createMenuItemOptionsMenuVideo() {
         final MenuButton videoMenuButton = new MenuButton("menu.video");
         videoMenuButton.setMenuContent(this::createContentVideo, this);
@@ -322,6 +323,7 @@ public class RelictusMenu extends FXGLMenu {
         return restoreMenuButton;
     }
 
+    // TODO: Trophäen Menü entfernen
     private MenuButton createMenuItemExtraMenuTrophies() {
         final MenuButton trophiesMenuButton = new MenuButton("menu.trophies");
         trophiesMenuButton.setMenuContent(this::createContentAchievements, this);
@@ -334,6 +336,7 @@ public class RelictusMenu extends FXGLMenu {
         return creditsMenuButton;
     }
 
+    // TODO: Feedback menü entfernen
     private MenuButton createMenuItemExtraMenuFeedback() {
         final MenuButton feedbackMenuButton = new MenuButton("menu.feedback");
         feedbackMenuButton.setMenuContent(this::createContentFeedback, this);
