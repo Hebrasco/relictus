df79f87f26f1bf00604f5d29c03157a1806f2868
Daniel Bedrich <hebrasco@icloud.com>
2019-02-25 18:37:48 +0100

    Adde UIPreferences for setting custom cursor

---
+++ src/main/java/utils/CustomCursor.java
@@ -0,0 +1,8 @@
+package utils;
+
+import javafx.geometry.Point2D;
+
+public class CustomCursor {
+    public static String defaultCurser = "cursor.png";
+    public static Point2D defaultHotSpot = new javafx.geometry.Point2D(0, 0);
+}
