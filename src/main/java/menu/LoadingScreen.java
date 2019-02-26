103e920e44bb3f17d16d9847d1d5a82b595f0306
Daniel Bedrich <hebrasco@icloud.com>
2019-02-26 18:05:59 +0100

    Added custom loading screen

---
+++ src/main/java/menu/LoadingScreen.java
@@ -0,0 +1,10 @@
+package menu;
+
+import com.almasb.fxgl.scene.LoadingScene;
+import utils.CustomCursor;
+
+public class LoadingScreen extends LoadingScene {
+    public LoadingScreen() {
+        setCursor(CustomCursor.defaultCurser, CustomCursor.defaultHotSpot);
+    }
+}
