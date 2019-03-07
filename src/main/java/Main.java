6cf61cc7ca210707bb4e29efd18c852bfa499c23
Daniel Bedrich <hebrasco@icloud.com>
2019-03-07 18:24:05 +0100

    Created JavaDoc for Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 import game.Relictus;
 
+/**
+ * The starting point of the relictus game.
+ * @author Kevin Ortmeier, Markus Kremer, Lara-Marie Mann, Roman Rubaskin, Daniel Bedrich
+ * @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
         Relictus.main(args);
