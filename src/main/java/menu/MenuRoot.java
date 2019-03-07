ba99bb416d6bdfb4abf87bfde5fea3d5a46f72ca
Daniel Bedrich <hebrasco@icloud.com>
2019-03-07 12:51:03 +0100

    Created JavaDoc for MenuRoot.java

--- src/main/java/menu/MenuRoot.java
+++ src/main/java/menu/MenuRoot.java
@@ -4,6 +4,7 @@ import javafx.scene.layout.VBox;
 
 /**
  * @author Daniel Bedrich
+ * @version 1.0
  */
 class MenuRoot extends VBox {
     MenuRoot(MenuButton... items) {
@@ -12,6 +13,10 @@ class MenuRoot extends VBox {
         }
     }
 
+    /**
+     * Adds the {@link MenuButton} to its Children
+     * @param item
+     */
     void add(MenuButton item) {
         item.setParent(this);
         getChildren().addAll(item);
