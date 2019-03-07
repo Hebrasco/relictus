a22409402bdb9fabf95c992b5f9324eb1c04292c
Daniel Bedrich <hebrasco@icloud.com>
2019-03-07 10:22:18 +0100

    Created JavaDoc for EntityTypes.java

--- src/main/java/data/EntityTypes.java
+++ src/main/java/data/EntityTypes.java
@@ -1,7 +1,10 @@
 package data;
 
 /**
+ * This enum contains all entity types for spawning into the game world.
+ *
  * @author Kevin Ortmeier
+ * @version 1.0
  */
 public enum EntityTypes {
     PLAYER, POWERUP, PLATFORM
