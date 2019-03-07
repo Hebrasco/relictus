38704971dad2eb7f6836b755bc89066b4084f2fa
Daniel Bedrich <hebrasco@icloud.com>
2019-03-07 11:17:19 +0100

    Created JavaDoc for PlayerComponent.java

--- src/main/java/game/components/PlayerComponent.java
+++ src/main/java/game/components/PlayerComponent.java
@@ -4,7 +4,11 @@ import com.almasb.fxgl.entity.component.Component;
 import game.player.PlayerControl;
 
 /**
+ * Adds all needed components to the player component and
+ * contains general information about the player.
+ *
  * @author Daniel Bedrich
+ * @version 1.0
  */
 public class PlayerComponent extends Component {
     public double playerWidth = 32;
