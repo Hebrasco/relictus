71154e06b55b4f1cb0d5e1cd57234ea9045f3605
Daniel Bedrich <hebrasco@icloud.com>
2019-03-07 10:26:01 +0100

    Removed unnecessary language name

--- src/main/java/data/Language.java
+++ src/main/java/data/Language.java
@@ -6,11 +6,9 @@ import java.util.ResourceBundle;
  * @author Daniel Bedrich
  */
 public class Language {
-    public String name;
     public ResourceBundle resourceBundle;
 
-    public Language(String name, ResourceBundle resourceBundle) {
-        this.name = name;
+    public Language(ResourceBundle resourceBundle) {
         this.resourceBundle = resourceBundle;
     }
 }
